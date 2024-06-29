d8c7deb4f2a5aef1bac998882c367ba1672ba658
youssef-khalifa <ykhalifa19921@gmail.com>
2024-06-29 21:22:59 +0300

    s

--- src/index.js
+++ src/index.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
 // import './index.css';
 // import App from './App';
- import StarRating from './StarRating';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import StarRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>this movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating />
+    <StarRating messages={["Terrible", "Bad", "Okay", "Good", "Amazing"]} />
+    <StarRating  size={'24'} color="red" defaultRating={3} />
+    <Test />
   </React.StrictMode>
 );
-
